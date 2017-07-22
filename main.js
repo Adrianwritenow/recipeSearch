2325e1a0e805ea66decf8cff146093bab8f88310
Adrian Rodriguez <adrianwritenow@gmail.com>
2017-07-22 19:18:31 -0400

    indentation fix

--- main.js
+++ main.js
@@ -1,14 +1,11 @@
 
 function foodSearch(){
   var userInput = document.getElementById("recipeSearch").value;
-
-
   fetch(`https://crossorigin.me/http://www.recipepuppy.com/api/?q=${userInput}`)
-
   .then(function(response){
     if (response.status !== 200) {
-    console.log(response.status);
-    return;
+      console.log(response.status);
+      return;
     }
     response.json().then(function(data){
       let main = document.querySelector(".main")
@@ -19,15 +16,15 @@ function foodSearch(){
       </div>
       <div class = foodContainer>
       `
-        for (var i = 0; i < data.results.length; i++) {
-          console.log(data.results[i].title);
-          template += `
-          <div clas = foodItem>
-            <a href="${data.results[i].href}"><img src = "${data.results[i].thumbnail}" onerror = "this.src ='https://pbs.twimg.com/media/CZiq32pUUAA_6pg.jpg'"</a>
-            <h3>${data.results[i].title}</h3>
-          </div>
-          `
-        }
+      for (var i = 0; i < data.results.length; i++) {
+        console.log(data.results[i].title);
+        template += `
+        <div clas = foodItem>
+          <a href="${data.results[i].href}"><img src = "${data.results[i].thumbnail}" onerror = "this.src ='https://pbs.twimg.com/media/CZiq32pUUAA_6pg.jpg'"</a>
+          <h3>${data.results[i].title}</h3>
+        </div>
+        `
+      }
       template += `</div>`;
       document.querySelector('body').innerHTML = template
     });
